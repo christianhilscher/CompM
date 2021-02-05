e71d7d0370ff7f3634721400a4f2783361c68f29
LCruzFer <lucas.cruz@hotmail.de>
2021-02-05 12:03:48 +0100

    adding written answer to last Q

--- A5/PS5.py
+++ A5/PS5.py
@@ -71,16 +71,19 @@ complete = actual_q.merge(forecasts, on = 'DATE')
 first, let's start with the dependent variable; 'shift' shifts the index x periods, so here shift 3 periods back such that the value of UNRATE 3 periods ahead is at the same index as the forecast for 3 periods ahead (namely, point t)
 '''
 complete['dependent'] = complete['UNRATE'].shift(-3) - complete['UNEMP5'] 
-#now, the independent variable, again using the shift() method, this time shift 1 period back (positive values imply shifting back)
-#we lose 1 period here obviously (the first one)
-#! THIS MIGHT NOT BE CORRECT BECAUSE WE WANT F_(t-1) x_(t+3) AS THE SECOND VARIABLE AND FOR NOW I AM USING THE FORECAST FROM t-1 3 PERIODS AHEAD WHICH SHOULD THEN BE x_(t+2), SO MAYBE WE ARE SUPPOSED TO USE UNEMP6 HERE IN T-1 
-complete['independent'] = complete['UNEMP5'] - complete['UNEMP5'].shift(1)
+'''
+Now the independent variable: the forecast of x_(t+3) in period t-1 is 4 (!) quarters ahead, not 3 (as a 3 quarters ahead forecast in t-1 is about t+2). Thus, use UNEMP6 and shift it by 1 period.
+We lose 1 period here obviously (the first one).
+'''
+complete['independent'] = complete['UNEMP5'] - complete['UNEMP6'].shift(1)
+sum(complete['independent'].isnull())
 
 #* 7.
 # Estimation
-form = 'independent ~ dependent'
+form = 'dependent ~ independent'
 results = regress(form, complete, cov = 'HAC')
 
 '''
-Interpretation of beta: how much does a change in forecast from t-1 to t change 
+Similarly as in Coibion and Gorodnynchenko (2015) (henceforth CG) we regress the aggregate forecast error of 3 quarters ahead forecasts on the forecast revisions of these forecasts within one period. The OLS coefficient is (assuming that it is unbiased) equal to the correlation between these two measures. Our results show that as in CG the coefficient is indeed significant at the 5% level. This suggests that there is a relationship between these two variables that under rational expectations should not exist. Under rational expecations we would assume that the updating process of the forecast from t-1 to t itself does not affect by how much the forecast errs. 
+Intuitively, the coefficient should show by how much the forecast error is changed when the forecast is updated by one unit and should therefore show some kind of measure for the deviation from the rationally expected updating behavior. 
 '''
